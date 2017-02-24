5e59e3fbdd98b7b596d5fea25ad6ffa876c2f3bd
Josh McAdams <joshua.mcadams@gmail.com>
2017-02-24 13:27:09 -0800

    Added some more files to create a skeletal sign-up flow

--- main.py
+++ main.py
@@ -8,7 +8,17 @@ class MainPage(webapp2.RequestHandler):
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.out.write(template.render(path, {}))
 
+class VolunteerPage(webapp2.RequestHandler):
+    def get(self):
+        path = os.path.join(os.path.dirname(__file__), 'volunteer_form.html')
+        self.response.out.write(template.render(path, {}))
+
+    def post(self):
+        path = os.path.join(os.path.dirname(__file__), 'thanks.html')
+        self.response.out.write(template.render(path, {}))
+
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/volunteer', VolunteerPage),
 ], debug=True)
